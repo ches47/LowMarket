689b2503233f2a25dc723b2468e55f2defd40bb7
Chesy <chesy47@gmail.com>
2021-02-03 17:48:57 +0200

    index half --DONE

--- app/js/main.js
+++ app/js/main.js
@@ -15,7 +15,32 @@ $('.product__slider-inner').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    dots: false
+    dots: false,
+    responsive: [
+        {
+          breakpoint: 1700,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
 });
     
 //Tabs(Card Page)    
