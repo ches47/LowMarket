f17e173fe340de9d05ef596ec54c6fcb6a7990e7
Ches <chesy47@gmail.com>
2021-03-27 07:02:02 +0200

    app/css

--- app/js/main.js
+++ app/js/main.js
@@ -122,7 +122,7 @@ window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
     var myDropdown = document.getElementById("myDropdown");
       if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
+        myDropdown.classList.remove('remove');
       }
   }
 }
